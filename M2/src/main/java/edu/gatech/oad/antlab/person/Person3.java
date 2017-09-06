a1b4d7859fe8ba9a3d975a5d40c07f5776e69982
jreno3 <joshuagregr@gmail.com>
2017-09-06 14:38:29 -0400

    fixed Person3.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,7 +43,7 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-        String string = "";
+        String string = input;
         if (input.length() == 0) {
             return string;
         }
